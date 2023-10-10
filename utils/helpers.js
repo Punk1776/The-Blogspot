6b1c3828f6a4a89b8c774492af4832c30c5963ea
unknown <gptv88@gmail.com>
2023-10-10 16:10:48 -0700

    make it better

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,6 +4,19 @@ function formatDate(date) {
       ).getFullYear()}`;
 }
 
+const Handlebars = require('handlebars');
+
+Handlebars.registerHelper('post-info', function (post) {
+    if (post && post.author && post.date) {
+        return `Posted by ${post.author} on ${formatDate(post.date)}`;
+    } else {
+        return 'Invalid or incomplete post data';
+    }
+
+    function formatDate(date) {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    }
+});
 
 module.exports = {
     formatDate 
